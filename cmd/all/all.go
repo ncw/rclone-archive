rclone/rclone

Implement mount2 with go-fuse FIXME WIP
  * Doesn't pass the tests yet
  * Fix seeking for readahead - see
    * https://github.com/rfjakob/gocryptfs/issues/92
    * https://github.com/rfjakob/gocryptfs/blob/master/internal/serialize_reads/sr.go

cmd/all/all.go
@@ -31,6 +31,7 @@ import (
 	_ "github.com/ncw/rclone/cmd/memtest"
 	_ "github.com/ncw/rclone/cmd/mkdir"
 	_ "github.com/ncw/rclone/cmd/mount"
+	_ "github.com/ncw/rclone/cmd/mount2"
 	_ "github.com/ncw/rclone/cmd/move"
 	_ "github.com/ncw/rclone/cmd/moveto"
 	_ "github.com/ncw/rclone/cmd/ncdu"

cmd/mount2/file.go
@@ -1,0 +1,120 @@
+// +build linux darwin freebsd
+
+package mount2
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/hanwen/go-fuse/fuse"
+	"github.com/hanwen/go-fuse/fuse/nodefs"
+	"github.com/ncw/rclone/fs/log"
+	"github.com/ncw/rclone/vfs"
+)
+
+// File represents a file
+type File struct {
+	h vfs.Handle
+}
+
+// Create a new File
+func newFile(h vfs.Handle) *File {
+	return &File{
+		h: h,
+	}
+}
+
+// Check interface satistfied
+var _ nodefs.File = (*File)(nil)
+
+// Called upon registering the filehandle in the inode.
+func (f *File) SetInode(i *nodefs.Inode) {
+}
+
+// The String method is for debug printing.
+func (f *File) String() string {
+	return fmt.Sprintf("fh=%p(%s)", f, f.h.Node().Path())
+}
+
+// Wrappers around other File implementations, should return
+// the inner file here.
+func (f *File) InnerFile() nodefs.File {
+	return nil
+}
+
+func (f *File) Read(dest []byte, off int64) (res fuse.ReadResult, code fuse.Status) {
+	var n int
+	var err error
+	defer log.Trace(f, "off=%d", off)("n=%d, status=%v", &n, &code)
+	n, err = f.h.ReadAt(dest, off)
+	if err == io.EOF {
+		err = nil
+	}
+	return fuse.ReadResultData(dest[:n]), translateError(err)
+}
+
+func (f *File) Write(data []byte, off int64) (written uint32, code fuse.Status) {
+	var n int
+	var err error
+	defer log.Trace(f, "off=%d", off)("n=%d, status=%v", &n, &code)
+	n, err = f.h.WriteAt(data, off)
+	return uint32(n), translateError(err)
+}
+
+// Flush is called for close() call on a file descriptor. In
+// case of duplicated descriptor, it may be called more than
+// once for a file.
+func (f *File) Flush() fuse.Status {
+	return translateError(f.h.Flush())
+}
+
+// This is called to before the file handle is forgotten. This
+// method has no return value, so nothing can synchronizes on
+// the call. Any cleanup that requires specific synchronization or
+// could fail with I/O errors should happen in Flush instead.
+func (f *File) Release() {
+	f.h.Release()
+}
+
+func (f *File) Fsync(flags int) (code fuse.Status) {
+	return translateError(f.h.Sync())
+}
+
+// The methods below may be called on closed files, due to
+// concurrency.  In that case, you should return EBADF.
+func (f *File) Truncate(size uint64) (code fuse.Status) {
+	defer log.Trace(f, "size=%d", size)("status=%v", &code)
+	return translateError(f.h.Truncate(int64(size)))
+}
+
+func (f *File) GetAttr(out *fuse.Attr) (code fuse.Status) {
+	defer log.Trace(f, "")("attr=%v, status=%v", &out, &code)
+	setAttr(f.h.Node(), out)
+	return fuse.OK
+}
+
+func (f *File) Chown(uid uint32, gid uint32) fuse.Status {
+	return translateError(f.h.Chown(int(uid), int(gid)))
+}
+
+func (f *File) Chmod(perms uint32) fuse.Status {
+	return translateError(f.h.Chmod(os.FileMode(perms)))
+}
+
+func (f *File) Utimens(atime *time.Time, mtime *time.Time) (code fuse.Status) {
+	defer log.Trace(f, "atime=%v, mtime=%v", atime, mtime)("status=%v", &code)
+	if mtime == nil {
+		return fuse.OK
+	}
+	return translateError(f.h.Node().SetModTime(*mtime))
+}
+
+func (f *File) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+
+func (f *File) Flock(flags int) fuse.Status {
+	return fuse.ENOSYS
+}

cmd/mount2/mount.go
@@ -1,0 +1,193 @@
+// Package mount implents a FUSE mounting system for rclone remotes.
+
+// +build linux darwin freebsd
+
+package mount2
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/fuse"
+	"github.com/hanwen/go-fuse/fuse/nodefs"
+	"github.com/hanwen/go-fuse/fuse/pathfs"
+	"github.com/ncw/rclone/cmd/mountlib"
+	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/vfs"
+	"github.com/okzk/sdnotify"
+	"github.com/pkg/errors"
+)
+
+const (
+	noisyDebug = false // set this to true for lots of noisy debug
+)
+
+func init() {
+	mountlib.NewMountCommand("mount2", Mount)
+}
+
+// mountOptions configures the options from the command line flags
+//
+// man mount.fuse for more info and note the -o flag for other options
+func mountOptions(fsys *FS, f fs.Fs) (mountOpts *fuse.MountOptions) {
+	device := f.Name() + ":" + f.Root()
+	mountOpts = &fuse.MountOptions{
+		AllowOther:    mountlib.AllowOther,
+		FsName:        device,
+		Name:          "rclone",
+		DisableXAttrs: true,
+		Debug:         mountlib.DebugFUSE,
+		MaxReadAhead:  int(mountlib.MaxReadAhead),
+	}
+	var opts []string
+	// FIXME doesn't work opts = append(opts, fmt.Sprintf("max_readahead=%d", maxReadAhead))
+	if mountlib.AllowNonEmpty {
+		opts = append(opts, "nonempty")
+	}
+	if mountlib.AllowOther {
+		opts = append(opts, "allow_other")
+	}
+	if mountlib.AllowRoot {
+		opts = append(opts, "allow_root")
+	}
+	if mountlib.DefaultPermissions {
+		opts = append(opts, "default_permissions")
+	}
+	if fsys.VFS.Opt.ReadOnly {
+		opts = append(opts, "ro")
+	}
+	if mountlib.WritebackCache {
+		log.Printf("FIXME --write-back-cache not supported")
+		// FIXME opts = append(opts,fuse.WritebackCache())
+	}
+	// Some OS X only options
+	if runtime.GOOS == "darwin" {
+		opts = append(opts,
+			// VolumeName sets the volume name shown in Finder.
+			fmt.Sprintf("volname=%s", device),
+
+			// NoAppleXattr makes OSXFUSE disallow extended attributes with the
+			// prefix "com.apple.". This disables persistent Finder state and
+			// other such information.
+			"noapplexattr",
+
+			// NoAppleDouble makes OSXFUSE disallow files with names used by OS X
+			// to store extended attributes on file systems that do not support
+			// them natively.
+			//
+			// Such file names are:
+			//
+			//     ._*
+			//     .DS_Store
+			"noappledouble",
+		)
+	}
+	mountOpts.Options = opts
+	return mountOpts
+}
+
+// mount the file system
+//
+// The mount point will be ready when this returns.
+//
+// returns an error, and an error channel for the serve process to
+// report an error when fusermount is called.
+func mount(f fs.Fs, mountpoint string) (*vfs.VFS, <-chan error, func() error, error) {
+	fs.Debugf(f, "Mounting on %q", mountpoint)
+
+	mountlib.DebugFUSE = true // FIXME
+
+	fsys := NewFS(f)
+	nodeFsOpts := &pathfs.PathNodeFsOptions{
+		ClientInodes: false,
+		Debug:        mountlib.DebugFUSE,
+	}
+	nodeFs := pathfs.NewPathNodeFs(fsys, nodeFsOpts)
+
+	mOpts := nodefs.NewOptions() // default options
+	// FIXME
+	// mOpts.EntryTimeout = 10 * time.Second
+	// mOpts.AttrTimeout = 10 * time.Second
+	// mOpts.NegativeTimeout = 10 * time.Second
+	mOpts.Debug = mountlib.DebugFUSE
+
+	conn := nodefs.NewFileSystemConnector(nodeFs.Root(), mOpts)
+	mountOpts := mountOptions(fsys, f)
+	//mountOpts := &fuse.MountOptions{}
+	server, err := fuse.NewServer(conn.RawFS(), mountpoint, mountOpts)
+
+	//server, _, err := nodefs.MountRoot(mountpoint, nodeFs.Root(), mOpts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	umount := server.Unmount
+
+	// serverSettings := server.KernelSettings()
+	// fs.Debugf(f, "Server settings %+v", serverSettings)
+
+	// Serve the mount point in the background returning error to errChan
+	errs := make(chan error, 1)
+	go func() {
+		server.Serve()
+		errs <- nil
+	}()
+
+	// wait for the mount point to be mounted
+	fs.Debugf(f, "Waiting for the mount to start...")
+	<-fsys.mounted
+
+	return fsys.VFS, errs, umount, nil
+}
+
+// Mount mounts the remote at mountpoint.
+//
+// If noModTime is set then it
+func Mount(f fs.Fs, mountpoint string) error {
+	// Mount it
+	vfs, errChan, unmount, err := mount(f, mountpoint)
+	if err != nil {
+		return errors.Wrap(err, "failed to mount FUSE fs")
+	}
+
+	sigInt := make(chan os.Signal, 1)
+	signal.Notify(sigInt, syscall.SIGINT, syscall.SIGTERM)
+	sigHup := make(chan os.Signal, 1)
+	signal.Notify(sigHup, syscall.SIGHUP)
+
+	if err := sdnotify.SdNotifyReady(); err != nil && err != sdnotify.SdNotifyNoSocket {
+		return errors.Wrap(err, "failed to notify systemd")
+	}
+
+waitloop:
+	for {
+		select {
+		// umount triggered outside the app
+		case err = <-errChan:
+			break waitloop
+		// Program abort: umount
+		case <-sigInt:
+			err = unmount()
+			break waitloop
+		// user sent SIGHUP to clear the cache
+		case <-sigHup:
+			root, err := vfs.Root()
+			if err != nil {
+				fs.Errorf(f, "Error reading root: %v", err)
+			} else {
+				root.ForgetAll()
+			}
+		}
+	}
+
+	_ = sdnotify.SdNotifyStopping()
+	if err != nil {
+		return errors.Wrap(err, "failed to umount FUSE fs")
+	}
+
+	return nil
+}

ncw/rclone-archive

cmd/all/all.go
@@ -36,6 +36,7 @@ import (
 	_ "github.com/rclone/rclone/cmd/memtest"
 	_ "github.com/rclone/rclone/cmd/mkdir"
 	_ "github.com/rclone/rclone/cmd/mount"
+	_ "github.com/rclone/rclone/cmd/mount2"
 	_ "github.com/rclone/rclone/cmd/move"
 	_ "github.com/rclone/rclone/cmd/moveto"
 	_ "github.com/rclone/rclone/cmd/ncdu"

cmd/mount2/file.go
@@ -1,0 +1,132 @@
+// +build linux darwin freebsd
+
+package mount2
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/hanwen/go-fuse/fuse"
+	"github.com/hanwen/go-fuse/fuse/nodefs"
+	"github.com/rclone/rclone/fs/log"
+	"github.com/rclone/rclone/vfs"
+)
+
+// File represents a file
+type File struct {
+	h vfs.Handle
+}
+
+// Create a new File
+func newFile(h vfs.Handle) *File {
+	return &File{
+		h: h,
+	}
+}
+
+// Check interface satistfied
+var _ nodefs.File = (*File)(nil)
+
+// Called upon registering the filehandle in the inode.
+func (f *File) SetInode(i *nodefs.Inode) {
+}
+
+// The String method is for debug printing.
+func (f *File) String() string {
+	return fmt.Sprintf("fh=%p(%s)", f, f.h.Node().Path())
+}
+
+// Wrappers around other File implementations, should return
+// the inner file here.
+func (f *File) InnerFile() nodefs.File {
+	return nil
+}
+
+func (f *File) Read(dest []byte, off int64) (res fuse.ReadResult, code fuse.Status) {
+	var n int
+	var err error
+	defer log.Trace(f, "off=%d", off)("n=%d, status=%v", &n, &code)
+	n, err = f.h.ReadAt(dest, off)
+	if err == io.EOF {
+		err = nil
+	}
+	return fuse.ReadResultData(dest[:n]), translateError(err)
+}
+
+func (f *File) Write(data []byte, off int64) (written uint32, code fuse.Status) {
+	var n int
+	var err error
+	defer log.Trace(f, "off=%d", off)("n=%d, status=%v", &n, &code)
+	n, err = f.h.WriteAt(data, off)
+	return uint32(n), translateError(err)
+}
+
+// Flush is called for close() call on a file descriptor. In
+// case of duplicated descriptor, it may be called more than
+// once for a file.
+func (f *File) Flush() fuse.Status {
+	return translateError(f.h.Flush())
+}
+
+// This is called to before the file handle is forgotten. This
+// method has no return value, so nothing can synchronizes on
+// the call. Any cleanup that requires specific synchronization or
+// could fail with I/O errors should happen in Flush instead.
+func (f *File) Release() {
+	f.h.Release()
+}
+
+func (f *File) Fsync(flags int) (code fuse.Status) {
+	return translateError(f.h.Sync())
+}
+
+// The methods below may be called on closed files, due to
+// concurrency.  In that case, you should return EBADF.
+func (f *File) Truncate(size uint64) (code fuse.Status) {
+	defer log.Trace(f, "size=%d", size)("status=%v", &code)
+	return translateError(f.h.Truncate(int64(size)))
+}
+
+func (f *File) GetAttr(out *fuse.Attr) (code fuse.Status) {
+	defer log.Trace(f, "")("attr=%v, status=%v", &out, &code)
+	setAttr(f.h.Node(), out)
+	return fuse.OK
+}
+
+func (f *File) Chown(uid uint32, gid uint32) fuse.Status {
+	return translateError(f.h.Chown(int(uid), int(gid)))
+}
+
+func (f *File) Chmod(perms uint32) fuse.Status {
+	return translateError(f.h.Chmod(os.FileMode(perms)))
+}
+
+func (f *File) Utimens(atime *time.Time, mtime *time.Time) (code fuse.Status) {
+	defer log.Trace(f, "atime=%v, mtime=%v", atime, mtime)("status=%v", &code)
+	if mtime == nil {
+		return fuse.OK
+	}
+	return translateError(f.h.Node().SetModTime(*mtime))
+}
+
+func (f *File) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+
+func (f *File) Flock(flags int) fuse.Status {
+	return fuse.ENOSYS
+}
+
+func (f *File) GetLk(owner uint64, lk *fuse.FileLock, flags uint32, out *fuse.FileLock) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+
+func (f *File) SetLk(owner uint64, lk *fuse.FileLock, flags uint32) (code fuse.Status) {
+	return fuse.ENOSYS
+}
+
+func (f *File) SetLkw(owner uint64, lk *fuse.FileLock, flags uint32) (code fuse.Status) {
+	return fuse.ENOSYS
+}

cmd/mount2/mount.go
@@ -1,0 +1,193 @@
+// Package mount implents a FUSE mounting system for rclone remotes.
+
+// +build linux darwin freebsd
+
+package mount2
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/fuse"
+	"github.com/hanwen/go-fuse/fuse/nodefs"
+	"github.com/hanwen/go-fuse/fuse/pathfs"
+	"github.com/okzk/sdnotify"
+	"github.com/pkg/errors"
+	"github.com/rclone/rclone/cmd/mountlib"
+	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/vfs"
+)
+
+const (
+	noisyDebug = false // set this to true for lots of noisy debug
+)
+
+func init() {
+	mountlib.NewMountCommand("mount2", Mount)
+}
+
+// mountOptions configures the options from the command line flags
+//
+// man mount.fuse for more info and note the -o flag for other options
+func mountOptions(fsys *FS, f fs.Fs) (mountOpts *fuse.MountOptions) {
+	device := f.Name() + ":" + f.Root()
+	mountOpts = &fuse.MountOptions{
+		AllowOther:    mountlib.AllowOther,
+		FsName:        device,
+		Name:          "rclone",
+		DisableXAttrs: true,
+		Debug:         mountlib.DebugFUSE,
+		MaxReadAhead:  int(mountlib.MaxReadAhead),
+	}
+	var opts []string
+	// FIXME doesn't work opts = append(opts, fmt.Sprintf("max_readahead=%d", maxReadAhead))
+	if mountlib.AllowNonEmpty {
+		opts = append(opts, "nonempty")
+	}
+	if mountlib.AllowOther {
+		opts = append(opts, "allow_other")
+	}
+	if mountlib.AllowRoot {
+		opts = append(opts, "allow_root")
+	}
+	if mountlib.DefaultPermissions {
+		opts = append(opts, "default_permissions")
+	}
+	if fsys.VFS.Opt.ReadOnly {
+		opts = append(opts, "ro")
+	}
+	if mountlib.WritebackCache {
+		log.Printf("FIXME --write-back-cache not supported")
+		// FIXME opts = append(opts,fuse.WritebackCache())
+	}
+	// Some OS X only options
+	if runtime.GOOS == "darwin" {
+		opts = append(opts,
+			// VolumeName sets the volume name shown in Finder.
+			fmt.Sprintf("volname=%s", device),
+
+			// NoAppleXattr makes OSXFUSE disallow extended attributes with the
+			// prefix "com.apple.". This disables persistent Finder state and
+			// other such information.
+			"noapplexattr",
+
+			// NoAppleDouble makes OSXFUSE disallow files with names used by OS X
+			// to store extended attributes on file systems that do not support
+			// them natively.
+			//
+			// Such file names are:
+			//
+			//     ._*
+			//     .DS_Store
+			"noappledouble",
+		)
+	}
+	mountOpts.Options = opts
+	return mountOpts
+}
+
+// mount the file system
+//
+// The mount point will be ready when this returns.
+//
+// returns an error, and an error channel for the serve process to
+// report an error when fusermount is called.
+func mount(f fs.Fs, mountpoint string) (*vfs.VFS, <-chan error, func() error, error) {
+	fs.Debugf(f, "Mounting on %q", mountpoint)
+
+	mountlib.DebugFUSE = true // FIXME
+
+	fsys := NewFS(f)
+	nodeFsOpts := &pathfs.PathNodeFsOptions{
+		ClientInodes: false,
+		Debug:        mountlib.DebugFUSE,
+	}
+	nodeFs := pathfs.NewPathNodeFs(fsys, nodeFsOpts)
+
+	mOpts := nodefs.NewOptions() // default options
+	// FIXME
+	// mOpts.EntryTimeout = 10 * time.Second
+	// mOpts.AttrTimeout = 10 * time.Second
+	// mOpts.NegativeTimeout = 10 * time.Second
+	mOpts.Debug = mountlib.DebugFUSE
+
+	conn := nodefs.NewFileSystemConnector(nodeFs.Root(), mOpts)
+	mountOpts := mountOptions(fsys, f)
+	//mountOpts := &fuse.MountOptions{}
+	server, err := fuse.NewServer(conn.RawFS(), mountpoint, mountOpts)
+
+	//server, _, err := nodefs.MountRoot(mountpoint, nodeFs.Root(), mOpts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	umount := server.Unmount
+
+	// serverSettings := server.KernelSettings()
+	// fs.Debugf(f, "Server settings %+v", serverSettings)
+
+	// Serve the mount point in the background returning error to errChan
+	errs := make(chan error, 1)
+	go func() {
+		server.Serve()
+		errs <- nil
+	}()
+
+	// wait for the mount point to be mounted
+	fs.Debugf(f, "Waiting for the mount to start...")
+	<-fsys.mounted
+
+	return fsys.VFS, errs, umount, nil
+}
+
+// Mount mounts the remote at mountpoint.
+//
+// If noModTime is set then it
+func Mount(f fs.Fs, mountpoint string) error {
+	// Mount it
+	vfs, errChan, unmount, err := mount(f, mountpoint)
+	if err != nil {
+		return errors.Wrap(err, "failed to mount FUSE fs")
+	}
+
+	sigInt := make(chan os.Signal, 1)
+	signal.Notify(sigInt, syscall.SIGINT, syscall.SIGTERM)
+	sigHup := make(chan os.Signal, 1)
+	signal.Notify(sigHup, syscall.SIGHUP)
+
+	if err := sdnotify.Ready(); err != nil && err != sdnotify.ErrSdNotifyNoSocket {
+		return errors.Wrap(err, "failed to notify systemd")
+	}
+
+waitloop:
+	for {
+		select {
+		// umount triggered outside the app
+		case err = <-errChan:
+			break waitloop
+		// Program abort: umount
+		case <-sigInt:
+			err = unmount()
+			break waitloop
+		// user sent SIGHUP to clear the cache
+		case <-sigHup:
+			root, err := vfs.Root()
+			if err != nil {
+				fs.Errorf(f, "Error reading root: %v", err)
+			} else {
+				root.ForgetAll()
+			}
+		}
+	}
+
+	_ = sdnotify.Stopping()
+	if err != nil {
+		return errors.Wrap(err, "failed to umount FUSE fs")
+	}
+
+	return nil
+}